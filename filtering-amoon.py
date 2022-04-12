00d26352a0f2bcd613f7972e74891ed6619c102d
HUCMRAN <101276424+HUCMRAN@users.noreply.github.com>
2022-04-13 00:19:45 +0430

    filtering-amoon.py

---
+++ filtering-amoon.py
@@ -0,0 +1,35 @@
+Skip to content
+
+Sign up
+
+mramon/filtering-amoon
+
+Code
+
+Issues
+
+Pull requests
+
+Actions
+
+Projects
+
+Wiki
+
+Security
+
+Insights
+
+ main 
+
+HUCMRAN/filtering-amoon
+
+
+
+mrramtin Add files via upload
+
+ 1 contributor
+
+516 lines (450 sloc)  20.3 KB
+
+# automatically installing libsimport osfrom os import *os.system('pip install colorama') import coloramafrom colorama import Fore, Back, Style, initinit() rami = ('amoonchp') # Amir the best in the internet :⁾ os.system('pip install time')import time os.system('clear')print (Fore.YELLOW+"[*]"+Fore.WHITE+"Installing python libraries" +Fore.BLUE+" please wait ...") # well go to installing os.system('pip install time')print (Fore.GREEN+'[*]'+Fore.WHITE+'Time library installed !')time.sleep(3)#os.system('pip install colored')#print (Fore.GREEN+'[*]'+Fore.WHITE+'colored library installed !')#time.sleep(3)os.system('pip install datetime')print (Fore.GREEN+'[*]'+Fore.WHITE+'datetime library installed !')time.sleep(3)os.system('pip install random')print (Fore.GREEN+'[*]'+Fore.WHITE+'random library installed !')time.sleep(3)#os.system('pip install tqdm')#print (Fore.GREEN+'[*]'+Fore.WHITE+'taqdm library installed !')#time.sleep(3)print (Fore.YELLOW+'[!]'+Fore.RED+'ok, well running script please wait' +Fore.BLUE+'(sabr kinid)')#importing libsimport time#import coloredimport datetimeimport osimport random#import tqdm#from colored import fg, bg, attr # code colorstime.sleep(3)red='\033[31m'green='\033[32m'blue='\033[36m'pink='\033[35m'rang='\033[34m'#$CRIPT_os.system("clear")while True: Mjie = input ("plz type password 🔑> ") if Mjie != "amoonchip": print(Fore.RED+'PASSWORD incorrect') continue else: break print (Fore.WHITE + "")#date()x = str(datetime.datetime.now()) # print time start print("started in " + (x))print('')print('')print('') bnr = [""" _ __ _
